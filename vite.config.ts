aff10bcad9f22cf0fac5071803b193fe8d088c46
Aes Sedai <zrlovin@gmail.com>
2023-02-10 21:49:29 -0800

    Add stats plus vendor chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { visualizer } from "rollup-plugin-visualizer"
+import { defineConfig, splitVendorChunkPlugin, type PluginOption } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react({ fastRefresh: true, include: "**/*.tsx" })],
+    plugins: [
+        react({ fastRefresh: true, include: "**/*.tsx" }),
+        visualizer() as PluginOption,
+        splitVendorChunkPlugin()
+    ],
     server: {
         port: 3000,
         host: "localhost"
